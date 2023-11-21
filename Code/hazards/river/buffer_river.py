4347bec75255788d08da927126c705d3ac47bc98
JoannaNoyes <149517920+JoannaNoyes@users.noreply.github.com>
2023-11-21 15:20:36 +0000

    Add files via upload

---
+++ Code/hazards/river/buffer_river.py
@@ -0,0 +1,38 @@
+from .river_list import river_list
+from ..third_party import Point, pd
+
+def river_buffer(*args, buffer_distance=0.003, river_cutoff=0.005):
+    """
+    Returns the river plot, polygon of an area and the river buffer for a chosen region. 
+    
+    Parameters:
+    -----------
+    *args: Union[str, Tuple[float, float, float, float]]
+        The positional arguments. This accepts either a single string 'location' value, which must be recognized as a region in OSM. Otherwise, 4 float arguments are accepted as 'north, south, east, west', defining a box for the chosen region
+    buffer_distance: float, Optional
+    	The buffer given to the river. This is in degrees. Default = 0.003 degrees.
+    river_cutoff: float, Optional
+    	The buffer given to the polygon of the chosen region outside of which river coordinates are dropped to provide a smaller dataset for the chosen region
+    	
+    Returns:
+    --------
+    river: geopandas.geodataframe.GeoDataFrame
+    	the geodataframe of the river profile, containing the geometry, name and new geometry
+    
+    polygon: shapely.geometry.polygon.Polygon
+    	polygon of the chosen region determined by args
+    
+    buffer_total: shapely.geometry.multipolygon.MultiPolygon
+    	polygon or multipolygon of the river buffers
+    
+    
+    """
+    river, polygon, buffered_polygon = river_list(*args, buffer=river_cutoff)
+    buffer_total = Point(0, 0).buffer(0)
+
+    for i in range(len(river)):
+        riv = river.loc[river.index[i], 'new geometry']
+        buffer = riv.buffer(buffer_distance)
+        buffer_total = buffer_total.union(buffer)
+
+    return river, polygon, buffer_total
---
+++ Code/hazards/river/river.py
@@ -0,0 +1,62 @@
+from ..third_party import ox, np, Polygon
+
+def river(*args): #, buffer = None):
+    """
+    Get rivers within the given region (box or area) through OpenStreetMaps (OSM), returning all rivers that intersect the chosen region as well as the Polygon of the region chosen
+    
+    Parameters
+    ----------
+    *args: Union[str, Tuple[float, float, float, float]]
+    	The positional arguments. This accepts either a single string 'location' value, which must be recognised as a region in OSM. Otherwise 4 float arguements are accepted as 'north, south, east, west', defining a box for the chosen region
+    		
+    Returns 
+    -------
+    river: geopandas.geodataframe.GeoDataFrame
+    	This is a list of the rivers contained within the region: their node id from OSM, their lat,lon node locations and the river name
+    polygon: shapely.geometry.polygon.Polygon
+    	A polygon of the given region
+    """
+
+    tags = {'waterway': 'river'}
+    river_data = []
+    #The following print the geometries ('geometries') and the geometry that is interested as a Polygon ('polygon'). 
+    #If no rivers are present in the location, the code will stop and not print out results
+    if len(args) == 1 and isinstance(args[0], str):
+        name = args[0]
+        print(f'Processing rivers in: {name}')
+        geometries = ox.features_from_place(name, tags=tags)
+        
+        shape = ox.features_from_place(name, tags={'name': name})
+        polygon = shape.unary_union
+                
+        if len(geometries) == 0:
+            print(f'No rivers found in {name}')
+            exit
+        else:
+            river_data.append((name, geometries))
+    elif len(args) == 4:
+        print('Processing rivers in lat long grid')
+        geometries = ox.features_from_bbox(args[0], args[1], args[2], args[3], tags=tags)
+        
+        polygon= Polygon([(args[3],args[1]),(args[3],args[0]),(args[2],args[0]),(args[2],args[1])])
+        
+        if len(geometries) == 0:
+            print('No rivers found in grid')
+            exit
+        else:
+            river_data.append(('LatLongGrid', geometries))
+            
+    #This is for when the wrong number of arguments is passed
+    else:
+        print('Argument is not callable. Either-')
+        print('        4 values: lat1, lat2, lon1, lon2')
+        print('        1 value: The name of a given area')
+        
+    unique_names = river_data[0][1]['name'].dropna().unique()
+    
+    print(river_data[0][1]['name'].nunique(), 'Unique Rivers Extracted:')
+    for value in unique_names:
+        print('      ', value)
+    return river_data[0][1][['geometry', 'name']], polygon
+
+   
